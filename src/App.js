aeb484568b9a2196d1aff9de322a8bf599228a8c
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-11-21 09:58:10 +0100

    Using setState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 /* eslint-disable no-useless-constructor */
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-import MyComponent from './components/MyComponent';
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
+import Counter from "./components/Counter";
+import Message from "./components/Message";
+import MyComponent from "./components/MyComponent";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.onClickBtn = this.onClickBtn.bind(this);
   }
 
   onClickBtn() {
-    console.log('Button has been clicked!');
+    console.log("Button has been clicked!");
   }
   render() {
     return (
       <div>
+        <Counter />
+        {/* <Message /> */}
         {/* <MyComponent title="React" onButtonClicked={this.onClickBtn} />
         <Welcome />
         <Hello name="Bruce">
@@ -31,7 +35,6 @@ class App extends Component {
         <Welcome name="Bruce" heroName="Batman" />
         <Welcome name="Clark" heroName="Superman" />
         <Welcome name="Diana" heroName="Wonder Woman" /> */}
-        <Message />
       </div>
     );
   }
